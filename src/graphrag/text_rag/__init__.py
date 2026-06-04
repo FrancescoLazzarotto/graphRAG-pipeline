4063ca4781a02a249e1f260e8095faa9759ca8af
Francesco Lazzarotto <checcolazzarotto@gmail.com>
2026-06-04 09:13:55 +0000

    Export dense text pipeline

--- src/graphrag/text_rag/__init__.py
+++ src/graphrag/text_rag/__init__.py
@@ -1,11 +1,15 @@
 from .agent import StandardRAGAgent
+from .dense_manager import DenseTextRAGManager
+from .factory import make_text_pipeline
 from .manager import TextChunk, TextRAGManager
 from .pipeline import RetrievedTextChunk, StandardTextRAGPipeline
 
 __all__ = [
+    "DenseTextRAGManager",
+    "make_text_pipeline",
+    "RetrievedTextChunk",
     "StandardRAGAgent",
+    "StandardTextRAGPipeline",
     "TextChunk",
     "TextRAGManager",
-    "RetrievedTextChunk",
-    "StandardTextRAGPipeline",
 ]
